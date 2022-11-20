0cd89a3f0b400e2d98ff533fc757a557aa01d90a
0xlay <zeroxlay@protonmail.com>
2022-11-20 15:43:04 +0200

    test(PassRipper::Crypto): add test for the AES_128_CBC

--- Tests/CryptoCipherTest.cpp
+++ Tests/CryptoCipherTest.cpp
@@ -1,6 +1,21 @@
 #include "gtest/gtest.h"
+#include "Crypto/Cipher.h"
+#include "Crypto/Cipher.cpp"
+#include "Crypto/Cipher/AES_128_CBC.h"
+#include "Crypto/Cipher/AES_128_CBC.cpp"
 
-TEST(SampleTest, Test)
+
+TEST(CryptoCipherTest, AES_128_CBC)
 {
-    ASSERT_TRUE(true);
+    constexpr char kPassword[] = "1234567890";
+    constexpr char kSource[] = "hello world";
+
+    std::string sourceEncrypted;
+    std::string sourceDecrypted;
+    Crypto::Cipher cipher = Crypto::Cipher(std::make_unique<Crypto::AES_128_CBC>(kPassword));
+
+    ASSERT_TRUE(cipher.encrypt(kSource, sourceEncrypted));
+    ASSERT_TRUE(cipher.decrypt(sourceEncrypted, sourceDecrypted));
+
+    ASSERT_EQ(kSource, sourceDecrypted);
 }
